f6c3d490017c8d308caa88ad71113bec245e6eaa
skubiak <sven.kubiak@maredit.de>
2015-04-15 09:44:29 +0200

    minor refactoring

--- src/main/java/de/maredit/tar/Main.java
+++ src/main/java/de/maredit/tar/Main.java
@@ -1,13 +1,9 @@
 package de.maredit.tar;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoDataAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
