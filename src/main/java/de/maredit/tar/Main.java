ce3920940da714e33b072734a99e2c408cf5ab3c
skubiak <sven.kubiak@maredit.de>
2015-04-15 08:12:46 +0200

    removed embedded mongodb dep for now

--- src/main/java/de/maredit/tar/Main.java
+++ src/main/java/de/maredit/tar/Main.java
@@ -1,25 +1,15 @@
 package de.maredit.tar;
 
-import javax.annotation.PreDestroy;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoDataAutoConfiguration;
 
-import de.svenkubiak.embeddedmongodb.EmbeddedMongo;
-
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class Main {
     public static void main(String[] args) {
-        EmbeddedMongo.DB.port(29019).start();
         SpringApplication.run(Main.class, args);
-    }
-    
-    @PreDestroy
-    public void shutdown() {
-        EmbeddedMongo.DB.stop();
     }
 }
