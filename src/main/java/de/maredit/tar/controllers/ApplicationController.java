c59c0cc1905d85b0eff1f5f85cd57eab0780221b
skubiak <sven.kubiak@maredit.de>
2015-04-16 15:34:49 +0200

    added shades plugin, removed unbound

--- src/main/java/de/maredit/tar/controllers/ApplicationController.java
+++ src/main/java/de/maredit/tar/controllers/ApplicationController.java
@@ -1,6 +1,7 @@
 package de.maredit.tar.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,8 +13,14 @@ public class ApplicationController {
     @Autowired
     private UserRepository userRepository;
     
+    @Autowired
+    private Environment environment;
+    
     @RequestMapping("/")
     public String index() {
+        System.out.println("Foo: " + environment.getProperty("spring.url"));
+        
+        
         return "application/index";
     }
 }
