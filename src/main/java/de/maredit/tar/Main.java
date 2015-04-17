e73865cb90e6ec60d63ea79be81a331b8c8fe856
skubiak <sven.kubiak@maredit.de>
2015-04-17 10:54:09 +0200

    added embedded mongodb on startup, refactored to single configuration file

--- src/main/java/de/maredit/tar/Main.java
+++ src/main/java/de/maredit/tar/Main.java
@@ -4,16 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import de.svenkubiak.embeddedmongodb.EmbeddedMongo;
+import de.maredit.tar.listeners.StartupListener;
 
 @SpringBootApplication
 @EnableScheduling
 public class Main {
     public static void main(String[] args) {
-        if (!("prod").equalsIgnoreCase(System.getProperty("spring.profiles.active"))) {
-            EmbeddedMongo.DB.port(28018).start();
-        }
-        
-        SpringApplication.run(Main.class, args);
+        SpringApplication springApplication = new SpringApplication(Main.class);
+        springApplication.addListeners(new StartupListener());
+        springApplication.run(args);
     }
 }
