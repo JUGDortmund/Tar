5bbd7ff1eca0eb87d9a9d976348ca0e229c02354
Pascal Plewa <pascal.plewa@maredit.de>
2015-04-20 08:50:55 +0200

    Fixed CSRF injection via Thymeleaf

--- src/main/java/de/maredit/tar/configs/AuthenticationConfig.java
+++ src/main/java/de/maredit/tar/configs/AuthenticationConfig.java
@@ -4,20 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import de.maredit.tar.providers.ApplicationAuthenticationProvider;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebMvcSecurity
 public class AuthenticationConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(final HttpSecurity http) throws Exception {
 		http
-		        .csrf()
-		        .disable()
 		        .authorizeRequests()
     				.antMatchers("/css/**").permitAll()
     				.antMatchers("/js/**").permitAll()
