8159876f99019384e2298ad011ccd0a72135c17e
okykal <okykal@mbp-okykal-lan.maredit.net>
2015-04-24 08:27:57 +0200

    TAR-89 Added dummy users to YAML file

--- src/main/java/de/maredit/tar/services/LdapDummyServiceImpl.java
+++ src/main/java/de/maredit/tar/services/LdapDummyServiceImpl.java
@@ -3,46 +3,58 @@ package de.maredit.tar.services;
 
 import com.unboundid.ldap.sdk.LDAPException;
 import de.maredit.tar.models.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @Profile("dev")
+@ConfigurationProperties(locations = "classpath:dummy-user.yaml")
 public class LdapDummyServiceImpl implements LdapService {
 
-  @Autowired
+  private List<User> users;
 
-  private static final Logger LOG = LoggerFactory.getLogger(LdapDummyServiceImpl.class);
+  private Map<String, String> authenticate;
 
-  @Override
-  public List<User> getUsers() throws LDAPException {
-    List<User> users = new ArrayList<>();
-    return users;
+  private Map<String, List<String>> groups;
 
+  public Map<String, String> getAuthenticate() {
+    return authenticate;
   }
 
-   @Override
+  public void setAuthenticate(Map<String, String> authenticate) {
+    this.authenticate = authenticate;
+  }
+
+  public Map<String, List<String>> getGroups() {
+    return groups;
+  }
+
+  public void setGroups(Map<String, List<String>> groups) {
+    this.groups = groups;
+  }
+
+  public void setUsers(List<User> users) {
+    this.users = users;
+  }
+
+  @Override
   public boolean authenticateUser(String uid, String password) throws LDAPException {
-    return "test".equals(uid) && "test".equals(password);
+    return password.equals(authenticate.get(uid));
   }
 
   @Override
   public List<String> getUserGroups(String uid) throws LDAPException {
-    List<String> groups = new ArrayList<>();
-    if ("user".equals(uid)) {
-      groups.add("userGroup");
-      groups.add("tar-user");
-    } else if ("supervisor".equals(uid)) {
-      groups.add("supervisorGroup");
-      groups.add("tar-supervisor");
-    }
-    return groups;
+    return groups.get(uid);
   }
-  
+
+  @Override
+  public List<User> getUsers() throws LDAPException {
+    return users;
+  }
+
+
 }
