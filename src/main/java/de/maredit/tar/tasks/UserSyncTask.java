31b41f9df5ec086c56a9ece843198b6fffe0350f
Pascal van den Brand Horninge <pascal.horninge@maredit.de>
2015-05-13 11:22:25 +0200

    TAR-133 changed encoding to utf8

--- src/main/java/de/maredit/tar/tasks/UserSyncTask.java
+++ src/main/java/de/maredit/tar/tasks/UserSyncTask.java
@@ -12,70 +12,75 @@ import org.springframework.stereotype.Component;
 
 import com.unboundid.ldap.sdk.LDAPException;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class UserSyncTask {
 
-  private static final Logger LOG = LogManager.getLogger(UserSyncTask.class);
+	private static final Logger LOG = LogManager.getLogger(UserSyncTask.class);
 
-  @Autowired
-  private LdapService ldapService;
+	@Autowired
+	private LdapService ldapService;
 
-  @Autowired
-  private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-  @Scheduled(cron = "0 0 */1 * * ?")
-  public void syncLdapUser() {
-    try {
-      LOG.debug("Syncing users");
-      List<User> ldapUserList = ldapService.getLdapUserList();
+	@Scheduled(cron = "0 0 */1 * * ?")
+	public void syncLdapUser() {
+		try {
+			LOG.debug("Syncing users");
+			List<User> ldapUserList = ldapService.getLdapUserList();
 
-      List<User> editedUser = new ArrayList<>();
-      for (User user : ldapUserList) {
-        User localUser = userRepository.findByUidNumber(user.getUidNumber());
-        if (localUser == null) {
-          localUser = user;
-          LOG.debug("User created. username: {} / uidNumber: {}", user.getUsername(),
-                    user.getUidNumber());
-        } else {
-          updateUser(localUser, user);
-          LOG.debug("User updated. username: {} / uidNumber: {}", user.getUsername(),
-                    user.getUidNumber());
-        }
-        userRepository.save(localUser);
-        editedUser.add(localUser);
-      }
+			List<User> editedUser = new ArrayList<>();
+			for (User user : ldapUserList) {
+				User localUser = userRepository.findByUidNumber(user
+						.getUidNumber());
+				if (localUser == null) {
+					localUser = user;
+					LOG.debug("User created. username: {} / uidNumber: {}",
+							user.getUsername(), user.getUidNumber());
+				} else {
+					updateUser(localUser, user);
+					LOG.debug("User updated. username: {} / uidNumber: {}",
+							user.getUsername(), user.getUidNumber());
+				}
+				userRepository.save(localUser);
+				editedUser.add(localUser);
+			}
 
-      // iterate over all users from repository and delete if they are not part of the LDAP
-      deactivateDeletedLdapUser(editedUser);
+			// iterate over all users from repository and delete if they are not
+			// part of the LDAP
+			deactivateDeletedLdapUser(editedUser);
 
-    } catch (LDAPException e) {
-      LOG.error("Failed to sync LDAP users", e);
-    }
-  }
+		} catch (LDAPException e) {
+			LOG.error("Failed to sync LDAP users", e);
+		}
+	}
 
+	private void deactivateDeletedLdapUser(List<User> ldapUsers) {
+		List<User> applicationUsers = userRepository.findAll();
 
-  private void deactivateDeletedLdapUser(List<User> ldapUsers) {
-    List<User> applicationUsers = userRepository.findAll();
+		// iterate over all users currently in the application
+		for (User applicationUser : applicationUsers) {
+			if (!ldapUsers.contains(applicationUser)) {
+				applicationUser.setActive(false);
+				userRepository.save(applicationUser);
+			}
+		}
+	}
 
-    // iterate over all users currently in the application
-    for (User applicationUser : applicationUsers) {
-      if (!ldapUsers.contains(applicationUser)) {
-        applicationUser.setActive(false);
-        userRepository.save(applicationUser);
-      }
-    }
-  }
-
-  private void updateUser(User resultEntry, User user) {
-    // set name changes here
-    user.setMail(resultEntry.getMail());
-    user.setUsername(resultEntry.getUsername());
-    user.setFirstname(resultEntry.getFirstname());
-    user.setLastname(resultEntry.getLastname());
-    user.setPhoto(resultEntry.getPhoto().getBytes());
-  }
-
+	private void updateUser(User resultEntry, User user) {
+		// set name changes here
+		user.setMail(resultEntry.getMail());
+		user.setUsername(resultEntry.getUsername());
+		user.setFirstname(resultEntry.getFirstname());
+		user.setLastname(resultEntry.getLastname());
+		try {
+			user.setPhoto(resultEntry.getPhoto().getBytes("UTF8"));
+		} catch (UnsupportedEncodingException e) {
+			LOG.error("Failed to sync utf-8 user photo", e);
+		}
+	}
 }
