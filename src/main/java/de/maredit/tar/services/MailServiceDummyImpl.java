32822838b7d7783d75257ea249c96c41a0df7db4
okykal <okykal@mbp-okykal-lan.maredit.net>
2015-05-15 14:08:31 +0200

    Fixed dummy services

--- src/main/java/de/maredit/tar/services/MailServiceDummyImpl.java
+++ src/main/java/de/maredit/tar/services/MailServiceDummyImpl.java
@@ -16,25 +16,16 @@ public class MailServiceDummyImpl implements MailService {
   private static final Logger LOG = LogManager.getLogger(MailServiceDummyImpl.class);
 
   @Autowired
-  private CustomMailProperties customMailProperties;
-
-  @Autowired
   private MailMessageComposer mailMessageComposer;
 
   @Override
   public void sendSimpleMail(MailObject mail) {
-    if(mail.sendToAdditionalRecipient()) {
-      mail.setCcRecipients(ArrayUtils.addAll(mail.getCCRecipients(), customMailProperties.getAdditionalRecipients()));
-    }
     LOG.info("Mail to be send:\n {}", mail.toString());
     LOG.info("Mail Text:\n {}", mailMessageComposer.composeSimpleMailMessage(mail).getText());
   }
 
   @Override
   public void sendMail(MailObject mail) {
-    if(mail.sendToAdditionalRecipient()) {
-      mail.setCcRecipients(ArrayUtils.addAll(mail.getCCRecipients(), customMailProperties.getAdditionalRecipients()));
-    }
     LOG.info("HTML mail to be send:\n {}", mail.toString());
     LOG.info("Mail Text:\n {}", mailMessageComposer.composeSimpleMailMessage(mail).getText());
   }
