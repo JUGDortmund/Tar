4b1a8956fffb271ac638f1d26f1dffe11cada4ae
Pascal van den Brand Horninge <pascal.horninge@maredit.de>
2015-05-19 16:33:25 +0200

    TAR-144 added DE/EN localization, changed frontend

---
+++ src/main/java/de/maredit/tar/configs/LocalizationConfig.java
@@ -0,0 +1,37 @@
+package de.maredit.tar.configs;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+
+@Configuration
+public class LocalizationConfig extends WebMvcConfigurerAdapter {
+
+  @Bean
+  public LocaleChangeInterceptor localeChangeInterceptor() {
+    LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+    localeChangeInterceptor.setParamName("language");
+    return localeChangeInterceptor;
+  }
+
+  @Bean(name = "localeResolver")
+  public CookieLocaleResolver localeResolver() {
+    CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+    Locale defaultLocale = new Locale("en");
+    localeResolver.setDefaultLocale(defaultLocale);
+    return localeResolver;
+  }
+
+  @Bean
+  public ResourceBundleMessageSource messageSource() {
+    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+    messageSource.setBasename("lang/messages");
+    return messageSource;
+  }
+}
