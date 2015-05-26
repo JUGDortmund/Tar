63c79ce57d07d89c7d38caefa31830b62ba2d2bb
Claudine Zillmann <claudine.zillmann@maredit.de>
2015-05-26 13:18:05 +0200

    flip panel

--- src/main/resources/static/plugins/flip/flip.js
+++ src/main/resources/static/plugins/flip/flip.js
@@ -1,4 +1,10 @@
     $('.flip').click(function(){
+
         $('.card').toggleClass('flipped');
+
+        setTimeout(function() {
+            $('.card .front').toggleClass('invisible');
+            $('.card .back').toggleClass('invisible');
+        },180);
 
     });
