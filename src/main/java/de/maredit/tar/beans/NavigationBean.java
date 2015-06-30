1ccdb7ee5139f6943a0ec566b651794661351557
Pascal van den Brand Horninge <pascal.horninge@maredit.de>
2015-06-30 08:47:17 +0200

    TAR-249 created datamodel

--- src/main/java/de/maredit/tar/beans/NavigationBean.java
+++ src/main/java/de/maredit/tar/beans/NavigationBean.java
@@ -5,6 +5,7 @@ public class NavigationBean {
   public static final String VACATION_PAGE = "vacation";
   public static final String CALENDAR_PAGE = "calendar";
   public static final String OVERVIEW_PAGE = "overview";
+  public static final String BOOKING_PAGE = "booking";
 
   private String activeComponent = VACATION_PAGE;
 
